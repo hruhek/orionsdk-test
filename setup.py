f484b530ffef7544cd78c8a5b163b32882c58bca
Hrvoje Ruhek <hrvoje.ruhek@ag04.com>
2021-05-20 14:48:41 +0200

    Added missing files

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='orionsdk-test',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='hruhek',
+    author_email='hruhek@ag04.com',
+    description='orionsdk test example'
+)
